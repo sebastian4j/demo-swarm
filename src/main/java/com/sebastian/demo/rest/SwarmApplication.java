db5dac380749fab3c59a0b7e398a1b14be8e34cb
Sebastian <sebastian4j@gmail.com>
2018-05-01 01:32:15 -0300

    modificaciones de nombres

---
+++ src/main/java/com/sebastian/demo/rest/SwarmApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.demo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author sebastian
+ */
+@ApplicationPath("/")
+public class SwarmApplication extends Application {
+    
+}
--- src/main/java/com/sebastian/demo/rest/SwarmRest.java
+++ src/main/java/com/sebastian/demo/rest/SwarmRest.java
@@ -1,13 +1,22 @@
 package com.sebastian.demo.rest;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import javax.json.Json;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
- *
+ * 
  * @author sebastian
  */
-@ApplicationPath("/")
-public class SwarmRest extends Application {
-    
+@Path("/hello")
+public class SwarmRest {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Saludo saludar() {
+        var s = new Saludo();
+        s.setMensaje("Hola!!");
+        return s;
+    }
 }
