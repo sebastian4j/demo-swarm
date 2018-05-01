08eda2137b2d24d2c1cc38e7b191c27c0c8a7e76
Sebastian <sebastian4j@gmail.com>
2018-05-01 01:24:42 -0300

    jax-rs hello world wildfly swarm microprofile

---
+++ src/main/java/com/sebastian/demo/rest/Saludo.java
@@ -0,0 +1,18 @@
+package com.sebastian.demo.rest;
+
+/**
+ *
+ * @author sebastian
+ */
+public class Saludo {
+    private String mensaje;
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
